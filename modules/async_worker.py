8c921c74537f24214ba02eaf21dd1df9d5f3b1dd
farhanshahriar07 <frhnshhrr@gmail.com>
2025-07-21 04:50:35 +0600

    modify

--- modules/async_worker.py
+++ modules/async_worker.py
@@ -339,9 +339,18 @@ def worker():
         for x in imgs:
             d = [('Prompt', 'prompt', task['log_positive_prompt']),
                  ('Negative Prompt', 'negative_prompt', task['log_negative_prompt']),
+                 ('Fooocus V2 Expansion', 'prompt_expansion', task['expansion']),
+                 ('Styles', 'styles',
+                  str(task['styles'] if not use_expansion else [fooocus_expansion] + task['styles'])),
                  ('Performance', 'performance', async_task.performance_selection.value),
                  ('Steps', 'steps', async_task.steps),
                  ('Resolution', 'resolution', str((width, height))),
+                 ('Guidance Scale', 'guidance_scale', async_task.cfg_scale),
+                 ('Sharpness', 'sharpness', async_task.sharpness),
+                 ('ADM Guidance', 'adm_guidance', str((
+                     modules.patch.patch_settings[pid].positive_adm_scale,
+                     modules.patch.patch_settings[pid].negative_adm_scale,
+                     modules.patch.patch_settings[pid].adm_scaler_end))),
                  ('Base Model', 'base_model', async_task.base_model_name),
                  ('Refiner Model', 'refiner_model', async_task.refiner_model_name),
                  ('Refiner Switch', 'refiner_switch', async_task.refiner_switch)]
@@ -357,6 +366,9 @@ def worker():
 
             if async_task.clip_skip > 1:
                 d.append(('CLIP Skip', 'clip_skip', async_task.clip_skip))
+            d.append(('Sampler', 'sampler', async_task.sampler_name))
+            d.append(('Scheduler', 'scheduler', async_task.scheduler_name))
+            d.append(('VAE', 'vae', async_task.vae_name))
             d.append(('Seed', 'seed', str(task['task_seed'])))
 
             if async_task.freeu_enabled:
@@ -374,7 +386,10 @@ def worker():
                                          task['log_negative_prompt'], task['negative'],
                                          async_task.steps, async_task.base_model_name, async_task.refiner_model_name,
                                          loras, async_task.vae_name)
-            
+            d.append(('Metadata Scheme', 'metadata_scheme',
+                      async_task.metadata_scheme.value if async_task.save_metadata_to_images else async_task.save_metadata_to_images))
+            d.append(('Version', 'version', 'Fooocus v' + fooocus_version.version))
+            img_paths.append(log(x, d, metadata_parser, async_task.output_format, task, persist_image))
 
         return img_paths
 
