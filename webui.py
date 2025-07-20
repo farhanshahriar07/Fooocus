57c0386c1db7adba339d75254226547873d79f88
farhanshahriar07 <frhnshhrr@gmail.com>
2025-07-21 04:27:51 +0600

    modify

--- webui.py
+++ webui.py
@@ -145,7 +145,7 @@ def inpaint_mode_change(mode, inpaint_engine_version):
 
 reload_javascript()
 
-title = f'Fooocus {fooocus_version.version}'
+title = f'AI Image Generation - Fooocus v{fooocus_version.version}'
 
 if isinstance(args_manager.args.preset, str):
     title += ' ' + args_manager.args.preset
