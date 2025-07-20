fb0403ad30ed535732da031e139996dad2bfc729
farhanshahriar07 <frhnshhrr@gmail.com>
2025-07-21 05:44:20 +0600

    modify

--- modules/config.py
+++ modules/config.py
@@ -441,7 +441,7 @@ default_max_image_number = get_config_item_or_set_default(
 )
 default_output_format = get_config_item_or_set_default(
     key='default_output_format',
-    default_value='png',
+    default_value='webp',
     validator=lambda x: x in OutputFormat.list(),
     expected_type=str
 )
