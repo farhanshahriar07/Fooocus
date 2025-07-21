8b11af7d04280403ea5bf34ec093dd386164853e
farhanshahriar07 <frhnshhrr@gmail.com>
2025-07-21 06:09:05 +0600

    modify

--- launch.py
+++ launch.py
@@ -150,3 +150,14 @@ config.update_files()
 init_cache(config.model_filenames, config.paths_checkpoints, config.lora_filenames, config.paths_loras)
 
 from webui import *
+
+shared.gradio_root.launch(
+    inbrowser=args_manager.args.in_browser,
+    server_name=args_manager.args.listen,
+    server_port=args_manager.args.port,
+    share=args_manager.args.share,
+    auth=check_auth if (args_manager.args.share or args_manager.args.listen) and auth_enabled else None,
+    allowed_paths=[modules.config.path_outputs],
+    blocked_paths=[constants.AUTH_FILENAME],
+    auth=("admin", "123"),
+)
--- webui.py
+++ webui.py
@@ -1120,13 +1120,13 @@ def dump_default_english_config():
 
 # dump_default_english_config()
 
-shared.gradio_root.launch(
-    inbrowser=args_manager.args.in_browser,
-    server_name=args_manager.args.listen,
-    server_port=args_manager.args.port,
-    share=args_manager.args.share,
-    auth=check_auth if (args_manager.args.share or args_manager.args.listen) and auth_enabled else None,
-    allowed_paths=[modules.config.path_outputs],
-    blocked_paths=[constants.AUTH_FILENAME],
-    auth=("admin", "123"),
-)
+# shared.gradio_root.launch(
+#     inbrowser=args_manager.args.in_browser,
+#     server_name=args_manager.args.listen,
+#     server_port=args_manager.args.port,
+#     share=args_manager.args.share,
+#     auth=check_auth if (args_manager.args.share or args_manager.args.listen) and auth_enabled else None,
+#     allowed_paths=[modules.config.path_outputs],
+#     blocked_paths=[constants.AUTH_FILENAME],
+#     auth=("admin", "123"),
+# )
